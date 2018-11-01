292cac7c5c60898ee31d1005147fc9433ed454a1
qiang <jhq0113@163.com>
2018-11-01 22:40:01 +0800

    demo

---
+++ yii-base/com/helpers/StringHelper.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jiang Haiqiang
+ * Date: 2018/11/1
+ * Time: 下午10:39
+ */
+
+namespace com\helpers;
+
+/**
+ * Class StringHelper
+ * @package com\helpers
+ * User Jiang Haiqiang
+ */
+class StringHelper extends \yii\helpers\StringHelper
+{
+
+}
