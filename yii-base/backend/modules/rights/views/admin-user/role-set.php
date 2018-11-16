971fc3937f3f645b259625f7640acb3b72b9e1f6
qiang <jhq0113@163.com>
2018-11-16 23:26:30 +0800

    权限管理

--- yii-base/backend/modules/rights/views/admin-user/role-set.php
+++ yii-base/backend/modules/rights/views/admin-user/role-set.php
@@ -13,15 +13,15 @@ use yii\widgets\ActiveForm;
 /* @var $model backend\models\AdminUser */
 /* @var $form yii\widgets\ActiveForm */
 
-$this->title = '角色管理';
+$this->title = '编辑管理员角色';
 $this->params['breadcrumbs'][] = ['label' => '管理员管理', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = '修改';
+$this->params['breadcrumbs'][] = '编辑管理员角色';
 
 ?>
 
 <div class="admin-user-role-set-form">
     <h1><?= Html::encode($this->title) ?></h1>
+    <hr/>
 
     <?php $form = ActiveForm::begin(); ?>
 
